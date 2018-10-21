97de0eed0e281503a46ac9b68b3f32271e473442
arthur silva <arxdsilva@gmail.com>
2018-10-21 02:07:46 -0300

    api: add implement home handler

--- api/server.go
+++ api/server.go
@@ -1,18 +1,24 @@
 package api
 
 import (
+	"net/http"
 	"os"
+	"strconv"
+	"time"
 
+	"github.com/arxdsilva/4thinkbe/db"
+	"github.com/arxdsilva/4thinkbe/models"
 	"github.com/labstack/echo"
+	"gopkg.in/mgo.v2"
 )
 
 type Server struct {
-	// Storage storage.Storage
-	Port string
+	Storage *mgo.Database
+	Port    string
 }
 
 func New() *Server {
-	return &Server{Port: port()}
+	return &Server{Port: port(), Storage: db.Connection()}
 }
 
 func (s *Server) Listen() {
@@ -30,6 +36,25 @@ func port() (p string) {
 }
 
 func (s *Server) HomeHandler(c echo.Context) (err error) {
-
-	return
+	userNumber := c.Param("userNumber")
+	number, err := strconv.Atoi(userNumber)
+	if err != nil {
+		return
+	}
+	p, err := models.GetByID(s.Storage, number)
+	if (err != nil) && (err != mgo.ErrNotFound) {
+		return
+	}
+	if err == mgo.ErrNotFound {
+		r := models.Room{Name: "Sala", Boxes: []models.Item{models.Item{Quantity: 2, Type: "moveis"}}}
+		ca := models.Address{Street: "Santa Luiza", Number: 259, Latitude: -22.9163398, Longitude: -43.2341546}
+		na := models.Address{Street: "Av Paulista", Number: 2537, Latitude: -23.5604276, Longitude: -46.6579269}
+		offer := models.Offer{Distance: 50}
+		p = models.Profile{ID: number, Inventory: r, NewAddress: na, CurrentAddress: ca, MovingData: time.Now(), MovingTime: time.Now(), Offer: offer}
+		errC := models.Create(s.Storage, p)
+		if errC != nil {
+			return errC
+		}
+	}
+	return c.JSON(http.StatusFound, p)
 }
